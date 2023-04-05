5bc2a550cb89f95bb4d2cb368a3b1128dafaa87e
Reinaldo Porto <reinaldoportoti@gmail.com>
2023-04-05 10:46:39 -0300

    exercise m1s08

--- login/exercicios/semana08/index.js
+++ login/exercicios/semana08/index.js
@@ -2,12 +2,39 @@ const connection = require('./src/database')
 const Equipamento = require('./src/models/equipamentos')
 
 connection.authenticate()
-connection.sync()
+connection.sync({alter: true})
 
-let insert = Equipamento.create({
-    name: "Esteira",
+try {
+    let insert = Equipamento.create({
+        name: "Esteira",
+        preco: 1500,
+        descricao: "Corrida"
+    })
     
-    descricao: "Corrida"
-})
+    console.log(insert)
+} catch (error) {
+    return response.status(404)
+            .json({ mensagem: "Não conseguimos processar sua solicitação." })
+}
 
-console.log(insert)
+
+try {
+    const updateEquipamento = await Equipamento.findByPk(request.params.id)
+    console.log(updateTask)
+    if (!updateEquipamento) {
+        return response.status(404)
+            .json({ mensagem: "Não conseguimos processar sua solicitação." })
+    }
+
+    updateEquipamento.name = request.body.name;
+    updateEquipamento.description = request.body.description
+
+    await updateEquipamento.save()
+
+    response.status(201).json(updateEquipamento)
+
+} catch (error) {
+    return response
+        .status(404)
+        .json({ mensagem: "Não conseguimos processar sua solicitação." })
+}
