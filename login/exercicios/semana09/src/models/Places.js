b31c1698097491336e31c6f7df298e6af325d073
Reinaldo Porto <reinaldoportoti@gmail.com>
2023-04-05 11:11:04 -0300

    exercise m1s09

---
+++ login/exercicios/semana09/src/models/Places.js
@@ -0,0 +1,35 @@
+const {Sequelize} = require('sequelize')
+const connection = require('../database')
+
+const Places = connection.define('places',{
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: Sequelize.STRING
+    },
+    openingHours: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING
+    },
+    latitude: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    longitude: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    }
+
+}
+
+)
