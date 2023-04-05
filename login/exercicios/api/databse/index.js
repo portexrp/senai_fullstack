6908e1c575691bbb64a73c8920cb3b104e00ae5f
Reinaldo Porto <63615053+portexrp@users.noreply.github.com>
2023-04-05 11:16:49 -0300

    Update index.js

--- login/exercicios/api/databse/index.js
+++ login/exercicios/api/databse/index.js
@@ -4,7 +4,7 @@ const connection = new Sequelize({
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: 'Rp@882101',
+    password: '123456',
     port: '5432',
     database: 'tarefas_database',
     define: {
