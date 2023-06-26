cf2bf434298b5bb000d6fc94644f163b5bdcac7f
Reinaldo Porto <reinaldoportoti@gmail.com>
2023-06-26 13:15:38 -0300

    [M2S04] Ex 7

--- login/exercicios/modulo02/semana04/asserts/js/index.js
+++ login/exercicios/modulo02/semana04/asserts/js/index.js
@@ -1,11 +1,17 @@
 
 let btnAlterarCorAtiva = document.getElementById('alterarCorAtiva')
-
+let content = document.getElementById('content')
+let h1Hheader = document.getElementById('h1Header')
 let btnColors = document.querySelectorAll('#alterColor'); // Adicione o seletor correto antes do nome da classe
 
 btnAlterarCorAtiva.addEventListener('click', () => {
+    content.style.background = '#fff'
+    h1Hheader.style.color= '#000000'
   btnColors.forEach((btn) => {
     btn.style.background = '#06bc37';
     console.log('passei aqui')
+    
   });
 });
+
+
